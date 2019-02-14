09e8ef9d4c05092af5aec2e738f61c1eec17c5fd
Kim Fairbanks <jdfairbanks@alaska.edu>
2019-02-13 16:23:23 -0900

    Added more to test.hpp

--- test.hpp
+++ test.hpp
@@ -4,25 +4,32 @@
 #include "string"
 #include "StringCalc.hpp"
 TEST_CASE ("An Empty String returns zero")
-    REQUIRE( kata_string() == 0);
+    REQUIRE( Add() == 0);
 TEST_CASE ("A single number returns the value")
+    REQUIRE(Add(a) == a );
+
+TEST_CASE("Two numbers, comma delimited, returns the sum")
+    REQUIRE(Add(a, b) == a+b );
+
+TEST_CASE("Two numbers, newline delimited, returns the sum")
     REQUIRE( );
 
-TEST_CASE("")
+TEST_CASE("Three numbers, delimited either way, returns the sum")
     REQUIRE( );
 
-TEST_CASE("")
+TEST_CASE("Negative numbers throw an exception")
     REQUIRE( );
 
-TEST_CASE("")
+TEST_CASE("Numbers greater than 1000 are ignored")
     REQUIRE( );
 
-TEST_CASE("")
+TEST_CASE("A single char delimiter can be defined on the first line (e.g. //# for a ‘#’ as the delimiter)")
     REQUIRE( );
 
-TEST_CASE("")
-    REQUIRE( );
+TEST_CASE("A multi char delimiter can be defined on the first line (e.g. //[###] for ‘###’ as the delimiter)")
+    REQUIRE
 
-TEST_CASE("")
-    REQUIRE( );
+TEST_CASE("Many single or multi-char delimiters can be defined (each wrapped in square brackets)")
+    REQUIRE
+
 #endif
