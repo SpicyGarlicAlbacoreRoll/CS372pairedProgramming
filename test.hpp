89997892396210bbf33326469d51417e4dfc00e5
Kim Fairbanks <jdfairbanks@alaska.edu>
2019-02-13 16:10:34 -0900

    added test header file

---
+++ test.hpp
@@ -0,0 +1,28 @@
+#define CATCH_TEST
+#ifndef CATCH_TEST
+#include "catch.hpp"
+#include "string"
+#include "StringCalc.hpp"
+TEST_CASE ("An Empty String returns zero")
+    REQUIRE( kata_string() == 0);
+TEST_CASE ("A single number returns the value")
+    REQUIRE( );
+
+TEST_CASE("")
+    REQUIRE( );
+
+TEST_CASE("")
+    REQUIRE( );
+
+TEST_CASE("")
+    REQUIRE( );
+
+TEST_CASE("")
+    REQUIRE( );
+
+TEST_CASE("")
+    REQUIRE( );
+
+TEST_CASE("")
+    REQUIRE( );
+#endif
