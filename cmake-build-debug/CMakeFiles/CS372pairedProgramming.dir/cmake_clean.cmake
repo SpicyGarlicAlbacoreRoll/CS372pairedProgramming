950c0844294069dd3b4b6231422b20d95e9efa92
Ian Ferguson <imferguson@alaska.edu>
2019-02-13 16:12:59 -0900

    Added Clion files (cmake) and StringCalc.cpp and .h

---
+++ cmake-build-debug/CMakeFiles/CS372pairedProgramming.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS372pairedProgramming.dir/StringCalc.cpp.o"
+  "CS372pairedProgramming.pdb"
+  "CS372pairedProgramming"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS372pairedProgramming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
